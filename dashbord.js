e7042ffcca526e3c5da5d237f17eca605986a096
stom-breaker-07 <chinmay.l10m8@gmail.com>
2023-12-05 23:48:00 +0530

    v1

---
+++ dashbord.js
@@ -0,0 +1,9 @@
+function show(){
+    var m=document.querySelector('.menu');
+    m.classList.add('smenu')
+}
+
+function cls(){
+    var m=document.querySelector('.menu');
+    m.classList.remove('smenu')
+}
