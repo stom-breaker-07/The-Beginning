e4bdb77ab119060960e78d3170a6e3809515527f
stom-breaker-07 <chinmay.l10m8@gmail.com>
2023-12-06 21:28:04 +0530

    add

--- dashbord.js
+++ dashbord.js
@@ -7,3 +7,8 @@ function cls(){
     var m=document.querySelector('.menu');
     m.classList.remove('smenu')
 }
+
+function big(){
+    var n=document.querySelector('.menu>ul>li>a');
+    n.clientHeight.add('.smenu>ul>li>a')
+}
